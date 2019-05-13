bf1feb0fd1a36d4b71e9460b69d18289d18f533a
joaosoaresaalm <joao.almeida@academico.ifpb.edu.br>
2019-05-13 18:28:31 -0300

    Update

--- InteligenciaArtificial/Grafo.py
+++ InteligenciaArtificial/Grafo.py
@@ -10,17 +10,7 @@ def Grafo():
        sep[2] = sep[2].replace('\n', "")
        G.add_edge(sep[0], sep[1], weight=float(sep[2]))
 
-
    return G
-
-def showGrafo(grafo):
-    plt.figure(1, figsize=(12, 8))             #definindo o tamanho da figura
-    pos=nx.fruchterman_reingold_layout(grafo)      #definindo o algoritmo do layout
-    plt.axis('off')                            #retira as bordas
-    nx.draw_networkx_nodes(grafo,pos,node_size=1000) #plota os nos
-    nx.draw_networkx_edges(grafo,pos,alpha=0.8)    #plota as arestas
-    plt.title('Cidades', size=16)     #Título
-    plt.show()
 
 def heuristicaDistancia():
     heuristica_csv = open('heuristicaDistancia.csv')
@@ -43,6 +33,5 @@ def heuristicaDistancia():
                 matDistancia[i][j] = None
 
     return ordenacaoCidades, matDistancia
-
 
 var = Grafo()
